22de758bec94f885a05e2a269712202ffae1ae73
ediciuStsaF <sherwoodroadster@googlemail.com>
2019-04-17 11:13:19 +0200

    Add files via upload

---
+++ ReadMe.txt
@@ -0,0 +1,10 @@
+Hello,
+
+for Alexa integration just make it with this excellent introductions.
+
+instead of the NodeRED_flows.txt you use Blume_NodeRed_import.txt for import... 
+and push implement.
+
+Then it should work...
+
+If  not, let me know..
---
+++ secrets.h
@@ -0,0 +1,10 @@
+// AP mode password
+
+//##############################################
+const char WiFiAPPSK[] = "";  // your password in AP mode
+
+// Wi-Fi network to connect to (if not in AP mode)
+ char* ssid = "";
+ char* password = "";
+
+//##############################################
